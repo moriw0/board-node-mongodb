801df1c06a27c149b28175203cd382c188a081b1
shota <tontonka2@gmail.com>
2022-03-22 08:41:44 +0900

    connect db

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const PASS = process.env.PASS;
+
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+mongoose
+  .connect(
+    `mongodb+srv://moriw0:${PASS}@cluster0.vifq9.mongodb.net/threads?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
 
-app.listen(PORT, console.log('server running'));
+app.listen(PORT, console.log("server running"));
