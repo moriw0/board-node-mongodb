6727817bc34e3f96b59f87ea557d0c46409a174c
shota <tontonka2@gmail.com>
2022-03-22 08:51:44 +0900

    create API

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
-
-const PASS = process.env.PASS;
-
 const PORT = 3000;
+const PASS = process.env.PASS;
+const Thread = require("./models/Thread");
 
 app.use(express.static("public"));
 
@@ -15,5 +14,23 @@ mongoose
   )
   .then(() => console.log("DB connected"))
   .catch((err) => console.log(err));
+
+app.get("/api/v1/threads", async (req, res) => {
+  try {
+    const allThreads = await Thread.find({});
+    res.status(200).json(allThreads);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/api/v1/thread", async (req, res) => {
+  try {
+    const createThread = await Thread.create(req.body);
+    res.status(200).json(createThread);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(PORT, console.log("server running"));
